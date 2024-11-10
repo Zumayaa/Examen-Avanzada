389bcbc685cf9e833b825a281e696c66fc73fffd
andreatrasvina <alucerotrasvina@gmail.com>
2024-11-09 23:58:36 -0700

    actualizacion

--- app/BrandController.php
+++ app/BrandController.php
@@ -1,42 +1,54 @@
-<?php 
-if (!isset($_SESSION)) {
-	session_start();
-}
-
+<?php
+session_start();
 class BrandController
 {
+  function get()
+  {
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+      CURLOPT_HTTPHEADER => array(
+        'Authorization: Bearer ' . $_SESSION['api_token'],
+      ),
+    ));
+    $response = curl_exec($curl);
+    $res_json = json_decode($response);
+    curl_close($curl);
+    if ($res_json->code == 4) {
+      return $res_json->data;
+    }
+    return [];
+  }
 
-
-	public function get()
-	{
-		$curl = curl_init();
-
-		curl_setopt_array($curl, array(
-		  CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands',
-		  CURLOPT_RETURNTRANSFER => true,
-		  CURLOPT_ENCODING => '',
-		  CURLOPT_MAXREDIRS => 10,
-		  CURLOPT_TIMEOUT => 0,
-		  CURLOPT_FOLLOWLOCATION => true,
-		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-		  CURLOPT_CUSTOMREQUEST => 'GET',
-		  CURLOPT_HTTPHEADER => array(
-		    'Authorization: Bearer '.$_SESSION['user_data']->token
-		  ),
-		));
-
-		$response = curl_exec($curl); 
-		curl_close($curl);
-		$response = json_decode($response);
-
-		if (isset($response->data) && count($response->data)) {
-			
-			return $response->data;
-		}
-
-		return array();
-	}
-
+  function getById($id){
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://crud.jonathansoto.mx/api/brands/' . $id,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+      CURLOPT_HTTPHEADER => array(
+        'Authorization: Bearer ' . $_SESSION['api_token'],
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    curl_close($curl);
+    
+    $decodedResponse = json_decode($response);
+    return $decodedResponse ? $decodedResponse->data : null;
+    
+    }
 }
-
-?>
