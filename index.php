8cbab27d418cb371a7319bfba54254677d4d2567
Haza Pro <hazaelfg@gmail.com>
2024-11-18 11:34:13 -0700

    login hecho

--- index.php
+++ index.php
@@ -59,18 +59,35 @@
                 <h4 class="f-w-500 mb-1">Inicia sesión con tu correo electrónico</h4>
                 <p class="mb-3">¿No tienes cuenta? <a class="link-primary ms-1">Ni modo</a></p>
               </div>
-              <div class="mb-3">
-                <input type="email" class="form-control" id="floatingInput" placeholder="Correo electrónico" />
-              </div>
-              <div class="mb-3">
-                <input type="password" class="form-control" id="floatingInput1" placeholder="Contraseña" />
-              </div>
-              <div class="d-grid mt-4">
-                <button type="button" class="btn btn-primary">Inicia sesión</button>
-              </div>
-              <div class="saprator my-3">
-                <span>No compartas tus accesos con nadie</span>
-              </div> 
+  						<form class="mt-5" method="POST" action="<?= BASE_PATH ?>app/AuthController.php">
+							  
+							  <div class="mb-3">
+							    <label for="exampleInputEmail1" class="form-label">
+							    	Correo electrónico
+							    </label>
+							    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="write here" aria-describedby="emailHelp" required name="correo">
+							    
+							    <div id="emailHelp" class="form-text">
+							    	Ingrese su correo electrónico registrado
+							    </div>
+							  </div>
+							  
+							  <div class="mb-3">
+							    <label for="exampleInputPassword1" class="form-label">
+							    	Contraseña
+							    </label>
+							    <input type="password" placeholder="* * * * * " class="form-control" id="exampleInputPassword1" required name="contrasenna">
+							  </div>
+							  	
+						  	<div class="d-grid gap-2"> 
+							  	<button type="submit" class="btn btn-primary">
+							  		Acceder
+							  	</button>
+							  </div>
+
+							 <input type="hidden" name="action" value="access">
+
+						  </form>
             </div>
           </div>
         </div>
